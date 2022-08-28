636e13ab718d197e0e066f668c8f35574eec2903
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2022-08-28 17:14:27 +0700

    Yes

---
+++ Mi To Km/Main.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+
+int main()
+{
+    double miles;
+
+    std::cout << "Input miles length: ";
+    std::cin >> miles;
+    
+    double km = miles * 1.609;
+
+    std::cout << miles << "Mil is " << km << "Km";
+
+    return 0;
+}
